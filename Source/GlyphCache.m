582451a8385434cc8859c08c5189b11a342b04d0
Luna Razzaghipour <luna@xoria.org>
2024-09-15 21:15:04 +1000

    Homogenize control flow in glyph cache method

--- Source/GlyphCache.m
+++ Source/GlyphCache.m
@@ -57,100 +57,99 @@ static const float padding = 2;
 	Assert(simd_all(subpixelOffset < 1));
 	Assert(simd_all(subpixelOffset >= 0));
 
+	GlyphCacheEntry *entry = NULL;
+
 	for (imm entryIndex = 0; entryIndex < entryCount; entryIndex++)
 	{
-		GlyphCacheEntry *entry = entries + entryIndex;
-		if (entry->glyph == glyph && CFEqual(entry->font, font) &&
-		        entry->subpixelOffset.x == subpixelOffset.x &&
-		        entry->subpixelOffset.y == subpixelOffset.y)
+		GlyphCacheEntry *candidate = entries + entryIndex;
+		if (candidate->glyph == glyph && CFEqual(candidate->font, font) &&
+		        simd_all(candidate->subpixelOffset == subpixelOffset))
 		{
-			CachedGlyph result = {0};
-			result.inset = padding;
-			result.textureCoordinatesBlack = entry->textureCoordinatesBlack;
-			result.textureCoordinatesWhite = entry->textureCoordinatesWhite;
-			result.size = entry->size;
-			return result;
+			entry = candidate;
+			break;
 		}
 	}
 
-	if (entryCount == entryCapacity)
+	if (entry == NULL)
 	{
-		entryCapacity *= 2;
-		entries = realloc(entries, (umm)entryCapacity * sizeof(GlyphCacheEntry));
-	}
-
-	GlyphCacheEntry *entry = entries + entryCount;
-	entryCount++;
-
-	CFRetain(font);
-	memset(entry, 0, sizeof(*entry));
-	entry->glyph = glyph;
-	entry->font = font;
-	entry->subpixelOffset = subpixelOffset;
-
-	simd_float2 boundingRectOrigin = 0;
-	simd_float2 boundingRectSize = 0;
-	{
-		CGRect boundingRect = {0};
-		CTFontGetBoundingRectsForGlyphs(
-		        font, kCTFontOrientationDefault, &glyph, &boundingRect, 1);
-		boundingRectOrigin.x = (float)boundingRect.origin.x;
-		boundingRectOrigin.y = (float)boundingRect.origin.y;
-		boundingRectSize.x = (float)boundingRect.size.width;
-		boundingRectSize.y = (float)boundingRect.size.height;
-		boundingRectOrigin *= scaleFactor;
-		boundingRectSize *= scaleFactor;
-	}
-
-	entry->size = ceil(boundingRectSize);
-	entry->size += 2 * padding;
-
-	for (bool32 black = 0; black <= 1; black++)
-	{
-		CFStringRef colorName = NULL;
-		if (black)
+		if (entryCount == entryCapacity)
 		{
-			colorName = kCGColorBlack;
+			entryCapacity *= 2;
+			entries = realloc(entries, (umm)entryCapacity * sizeof(GlyphCacheEntry));
 		}
-		else
-		{
-			colorName = kCGColorWhite;
-		}
-		CGContextSetFillColorWithColor(context, CGColorGetConstantColor(colorName));
+		entry = entries + entryCount;
+		entryCount++;
 
-		if (cursor.x + boundingRectSize.x + 2 * padding >= diameter)
+		CFRetain(font);
+		memset(entry, 0, sizeof(*entry));
+		entry->glyph = glyph;
+		entry->font = font;
+		entry->subpixelOffset = subpixelOffset;
+
+		simd_float2 boundingRectOrigin = 0;
+		simd_float2 boundingRectSize = 0;
 		{
-			cursor.x = 0;
-			cursor.y += ceil(largestGlyphHeight) + 2 * padding;
-			largestGlyphHeight = 0;
+			CGRect boundingRect = {0};
+			CTFontGetBoundingRectsForGlyphs(
+			        font, kCTFontOrientationDefault, &glyph, &boundingRect, 1);
+			boundingRectOrigin.x = (float)boundingRect.origin.x;
+			boundingRectOrigin.y = (float)boundingRect.origin.y;
+			boundingRectSize.x = (float)boundingRect.size.width;
+			boundingRectSize.y = (float)boundingRect.size.height;
+			boundingRectOrigin *= scaleFactor;
+			boundingRectSize *= scaleFactor;
 		}
 
-		if (black)
-		{
-			entry->textureCoordinatesBlack = cursor;
-		}
-		else
-		{
-			entry->textureCoordinatesWhite = cursor;
-		}
+		entry->size = ceil(boundingRectSize);
+		entry->size += 2 * padding;
 
+		for (bool32 black = 0; black <= 1; black++)
 		{
-			simd_float2 drawPosition = cursor;
-			drawPosition -= boundingRectOrigin;
-			drawPosition += subpixelOffset;
-			drawPosition += padding;
-			drawPosition /= scaleFactor;
-			CGPoint drawPositionCG = {drawPosition.x, drawPosition.y};
-			CTFontDrawGlyphs(font, &glyph, &drawPositionCG, 1, context);
+			CFStringRef colorName = NULL;
+			if (black)
+			{
+				colorName = kCGColorBlack;
+			}
+			else
+			{
+				colorName = kCGColorWhite;
+			}
+			CGContextSetFillColorWithColor(context, CGColorGetConstantColor(colorName));
+
+			if (cursor.x + boundingRectSize.x + 2 * padding >= diameter)
+			{
+				cursor.x = 0;
+				cursor.y += ceil(largestGlyphHeight) + 2 * padding;
+				largestGlyphHeight = 0;
+			}
+
+			if (black)
+			{
+				entry->textureCoordinatesBlack = cursor;
+			}
+			else
+			{
+				entry->textureCoordinatesWhite = cursor;
+			}
+
+			{
+				simd_float2 drawPosition = cursor;
+				drawPosition -= boundingRectOrigin;
+				drawPosition += subpixelOffset;
+				drawPosition += padding;
+				drawPosition /= scaleFactor;
+				CGPoint drawPositionCG = {drawPosition.x, drawPosition.y};
+				CTFontDrawGlyphs(font, &glyph, &drawPositionCG, 1, context);
+			}
+
+			[texture replaceRegion:MTLRegionMake2D(0, 0, (umm)diameter, (umm)diameter)
+			           mipmapLevel:0
+			             withBytes:CGBitmapContextGetData(context)
+			           bytesPerRow:(umm)diameter];
+
+			largestGlyphHeight = Max(largestGlyphHeight, boundingRectSize.y);
+			cursor.x += ceil(boundingRectSize.x) + 2 * padding;
 		}
-
-		[texture replaceRegion:MTLRegionMake2D(0, 0, (umm)diameter, (umm)diameter)
-		           mipmapLevel:0
-		             withBytes:CGBitmapContextGetData(context)
-		           bytesPerRow:(umm)diameter];
-
-		largestGlyphHeight = Max(largestGlyphHeight, boundingRectSize.y);
-		cursor.x += ceil(boundingRectSize.x) + 2 * padding;
 	}
 
 	CachedGlyph result = {0};
